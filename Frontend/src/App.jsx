01e58a9405f972c4bbae6dfd5ed535751aa4f389
Abhishekkumar21 <Abhishek.ak656@gmail.com>
2024-06-05 23:24:20 +0530

    Imported Banner.jsx to App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import Banner from "./Components/Banner";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Banner />
     </>
   );
 }
