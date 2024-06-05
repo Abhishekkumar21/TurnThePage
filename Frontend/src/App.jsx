f2251bbd99dc4ead46ff27b9b41a6f8bb6e52db1
Abhishekkumar21 <Abhishek.ak656@gmail.com>
2024-06-06 02:00:44 +0530

    imported FreeBook to App

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./Components/Navbar";
 import Banner from "./Components/Banner";
 import Footer from "./Components/Footer";
+import FreeBook from "./Components/FreeBook";
 
 function App() {
   return (
     <>
       <Navbar />
       <Banner />
+      <FreeBook />
       <Footer />
     </>
   );
