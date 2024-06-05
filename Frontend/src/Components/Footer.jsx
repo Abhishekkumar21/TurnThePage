e4c113a1d2911e5b1654590f8697085dd8cab97d
Abhishekkumar21 <Abhishek.ak656@gmail.com>
2024-06-05 23:57:14 +0530

    Updated footer bg color and added a horizontal line on top of it

--- Frontend/src/Components/Footer.jsx
+++ Frontend/src/Components/Footer.jsx
@@ -4,7 +4,8 @@ function Footer() {
   return (
     <>
       <div>
-        <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+        <hr />
+        <footer className="footer footer-center p-10 bg-base-100 text-base-content rounded">
           <nav className="grid grid-flow-col gap-4">
             <a className="link link-hover">About us</a>
             <a className="link link-hover">Contact</a>
