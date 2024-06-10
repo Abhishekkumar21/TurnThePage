b2b648d493ace1467f750403e8ff6b84fe6fde2d
Abhishekkumar21 <Abhishek.ak656@gmail.com>
2024-06-10 22:41:27 +0530

    Updated: card's details

--- Frontend/src/Components/Card.jsx
+++ Frontend/src/Components/Card.jsx
@@ -12,7 +12,7 @@ function Card({ item }) {
           <div className="card-body">
             <h2 className="card-title">
               {item.name}
-              <div className="badge badge-secondary">free</div>
+              <div className="badge badge-secondary">{item.category}</div>
             </h2>
             <p>{item.title}</p>
             <div className="card-actions justify-between">
