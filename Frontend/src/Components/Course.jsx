2f23371677664abc64191339d359e3e5820edeff
Abhishekkumar21 <Abhishek.ak656@gmail.com>
2024-06-11 00:25:11 +0530

    Added course page contents

--- Frontend/src/Components/Course.jsx
+++ Frontend/src/Components/Course.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import freebooklist from "../../public/freebooklist.json";
+import Card from "./Card";
 
 function Course() {
-  return <>This is course page!!</>;
+  console.log(freebooklist);
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div className="mt-28 justify-center items-center text-center">
+          <h1 className=" text-2xl md:text-4xl font-semibold">
+            Welcome to our collection !
+          </h1>
+          <p className=" mt-10">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum
+            maiores natus fuga corporis dolorem aliquam. Expedita, dolorem hic
+            sapiente aperiam velit fugit odit ducimus laboriosam dolorum earum
+            minima explicabo porro.
+          </p>
+          <div className="mt-10 grid gap-3 grid-cols-1  md:grid-cols-4">
+            {freebooklist.map((item) => (
+              <Card item={item} key={item.id} />
+            ))}
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default Course;
