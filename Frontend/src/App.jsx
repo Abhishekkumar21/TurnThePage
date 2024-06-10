e25a5e469b1beeffe79b7f7b913a773fb0a604a9
Abhishekkumar21 <Abhishek.ak656@gmail.com>
2024-06-10 22:44:02 +0530

    Added: Routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Home from "./home/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Course from "./Components/Course";
 
 function App() {
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/course" element={<Course />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
